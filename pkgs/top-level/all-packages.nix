00098a3b647b19d2d16958ed244405735f795747
ajs124 <git@ajs124.de>
2022-05-31 23:37:19 +0200

    lighttpd: pin to openssl_1_1
    tests fail with openssl_3_0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22282,7 +22282,9 @@ with pkgs;
     nodejs = nodejs-14_x;
   };
 
-  lighttpd = callPackage ../servers/http/lighttpd { };
+  lighttpd = callPackage ../servers/http/lighttpd {
+    openssl = openssl_1_1;
+  };
 
   livepeer = callPackage ../servers/livepeer { };
 
