4ebcd7c4cba74f8bcb5493b6cd44be8389a721ce
ajs124 <git@ajs124.de>
2022-07-04 18:18:21 +0200

    tengine: pin to openssl_1_1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -32860,6 +32860,7 @@ with pkgs;
   teeworlds = callPackage ../games/teeworlds { };
 
   tengine = callPackage ../servers/http/tengine {
+    openssl = openssl_1_1;
     modules = with nginxModules; [ rtmp dav moreheaders modsecurity-nginx ];
   };
 
